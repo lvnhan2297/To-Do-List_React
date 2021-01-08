94cfa167a2570d86b57230407c8da22d6d5719ae
levannhan228 <lvnhan0897@gmail.com>
2021-01-08 08:18:15 +0700

    todolist done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import './App.css';
 import React , {useState} from 'react';
+
 const App = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([ {
+    text: 'Share hööks with the bretheren',
+    isComplete: false
+  },
+  {
+    text: 'Ask bröther if you may have höök',
+    isComplete: false
+  }]);
+
   const [dataInput,setDataInput] = useState('');
   const handleChange = (e) =>{
     setDataInput(e.target.value);
   }
   const handleClick = (e) =>{
     e.preventDefault();
-    setTodos([dataInput,...todos]);
+    setTodos([{text:dataInput,isComplete: false},...todos]);
     setDataInput('');
   }
+  const removeItem = (index,e) => {
+    e.stopPropagation();
+    const newTasks = [...todos];
+    newTasks.splice(index, 1);
+    console.log(newTasks);
+    setTodos(newTasks);
+  }
+
+  const handleComplete = index => {
+    const newTodos = [...todos];
+    newTodos[index].isComplete = !newTodos[index].isComplete;
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <h1>Todo List App</h1>
       <form>
-      <input type="text" value={dataInput} onChange={handleChange}/>
-      <button onClick={handleClick}>add to do</button>
+        <input type="text" value={dataInput} onChange={handleChange}/>
+        <button onClick={handleClick}>add to do</button>
       </form>
       <ul>
-        {todos.map((item,i)=>
-          <li key={i}>{item}</li>
+      
+        {todos.map(({text,isComplete},index)=>
+          <li key={index}  className={isComplete ? 'complete' : null} onClick={handleComplete.bind(this, index)} >{text} 
+          <button onClick={removeItem.bind(this, index)}>remove</button></li>
         )}
       </ul>
     </div>
